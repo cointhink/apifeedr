124ca1410f6c877b2278d9c47c3b20459669788c
Don Park <don@donpark.org>
2013-03-22 14:38:55 -0700

    more clumsy moduleization

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-var fs = require('fs');
-var bouncy = require('bouncy');
-var redisLib = require("redis");
-var uuid = require('node-uuid');
 var worker = require('./lib/worker')
-var queue = {}//require('./lib/queue')
+var queue = require('./lib/queue')
 
 apifeedr = { worker: worker,
              queue: queue }
 
-exports = module.exports = apifeedr
+module.exports = apifeedr
+
