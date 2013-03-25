7bf7ead4e1bf6bd7e7aa21f7f861105887afebb8
Don Park <don@donpark.org>
2013-03-25 14:02:30 -0700

    long ways to go with tests

--- test/worker.js
+++ test/worker.js
@@ -2,17 +2,17 @@ var workerLib = require('../lib/worker.js')
 var events = require('events')
 
 describe("apifeedr worker", function() {
-  it("contains spec with an expectation", function() {
+  it("starts listening for jobs", function() {
     spyOn(workerLib, 'work_jobs')
     workerLib.work(function(){})
     expect(workerLib.work_jobs).toHaveBeenCalled()
   });
 
-  it("handles a job", function(){
+  it("handles the queue", function(){
     var finisher = new events.EventEmitter()
     spyOn(workerLib, 'work_next_job')
     workerLib.work_jobs(function(){}, finisher)
-    // nothing to test?
+    // queue is always empty
   })
 });
 
