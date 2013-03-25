c7ba39e0f95d3d40d9fa0545518ec658f7cbcf1a
Don Park <don@donpark.org>
2013-03-25 13:52:23 -0700

    tests expose api weakness

--- test/worker.js
+++ test/worker.js
@@ -1,4 +1,5 @@
 var workerLib = require('../lib/worker.js')
+var events = require('events')
 
 describe("apifeedr worker", function() {
   it("contains spec with an expectation", function() {
@@ -6,5 +7,12 @@ describe("apifeedr worker", function() {
     workerLib.work(function(){})
     expect(workerLib.work_jobs).toHaveBeenCalled()
   });
+
+  it("handles a job", function(){
+    var finisher = new events.EventEmitter()
+    spyOn(workerLib, 'work_next_job')
+    workerLib.work_jobs(function(){}, finisher)
+    // nothing to test?
+  })
 });
 
